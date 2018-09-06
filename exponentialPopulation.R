484de14cd307b09a3c475ebb0be541ee1cc1cf7d
Gabriel Teotonio <gabrielhfidelis@hotmail.com>
2018-09-05 23:05:07 -0300

    Update exponentialPopulation.R

--- exponentialPopulation.R
+++ exponentialPopulation.R
@@ -1,6 +1,6 @@
 m <- 1000            #number of replicas
 mu_verd <- 1/2       #true value of the population parameter
-mu_test <- c(seq(1/16,4,0.1825))   #test values
+mu_test <- c(0.1,0.15,0.2,0.25,0.3,0.4,.5,1,1.5,2,2.5,3,3.5,4,4.5,5)   #test values
 M <- length(mu_test)              #number of Monte Carlo replicas
 power <- numeric(M)               #vector to storage the empirrical power of the test
 nobs <- c(20,30,40,50,100)        #sampling size
@@ -10,7 +10,7 @@ for(j in nobs) {
   for (i in 1:M) {
     mu <- mu_test[i]
     p_value <- replicate(m, expr = {
-      x <- rexp(j, 2)
+      x <- rexp(j, 1/mu)
       test_t <- t.test(x, alternative = "greater", mu = mu_verd)
       test_t$p.value})
     power[i] <- mean(p_value <= 0.05)
