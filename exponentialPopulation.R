fc9d2ad96b09ee85ea8560a3bf3e50e5b7d963bb
Gabriel Teotonio <gabrielhfidelis@hotmail.com>
2018-09-07 19:49:45 -0300

    Update exponentialPopulation.R

--- exponentialPopulation.R
+++ exponentialPopulation.R
@@ -3,8 +3,8 @@ mu_verd <- 1/2       #true value of the population parameter
 mu_test <- c(0.1,0.15,0.2,0.25,0.3,0.4,.5,1,1.5,2,2.5,3,3.5,4,4.5,5)   #test values
 M <- length(mu_test)              #number of Monte Carlo replicas
 power <- numeric(M)               #vector to storage the empirrical power of the test
-nobs <- c(20,30,40,50,100)        #sampling size
-power_nobs <- matrix(0,length(nobs),M)
+nobs <- c(20, 40, 50, 100)        #sampling size
+power_nobs <- matrix(0,length(nobs),M) #matrix to storage the empirical power of the test for each n
 c <- 1
 for(j in nobs) {
   for (i in 1:M) {
@@ -18,3 +18,18 @@ for(j in nobs) {
   power_nobs[c,] <- power
   c = c+1
 }
+
+
+par(mfrow=c(2,2))
+plot(mu_test, power_nobs[1,], type = "l", xlab = bquote(theta), ylab = "Power", main = "n = 20")
+abline(v = mu_verd, lty = 1)
+abline(h = .05, lty = 1)
+plot(mu_test, power_nobs[2,], type = "l", xlab = bquote(theta), ylab = "Power", main = "n = 40")
+abline(v = mu_verd, lty = 1)
+abline(h = .05, lty = 1)
+plot(mu_test, power_nobs[3,], type = "l", xlab = bquote(theta), ylab = "Power", main = "n = 50")
+abline(v = mu_verd, lty = 1)
+abline(h = .05, lty = 1)
+plot(mu_test, power_nobs[4,], type = "l", xlab = bquote(theta), ylab = "Power", main = "n = 100")
+abline(v = mu_verd, lty = 1)
+abline(h = .05, lty = 1)
