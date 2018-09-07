c7cd305da799a830b433a54f79716ee3462db202
Gabriel Teotonio <gabrielhfidelis@hotmail.com>
2018-09-07 19:50:34 -0300

    Update exponentialPopulation.R

--- exponentialPopulation.R
+++ exponentialPopulation.R
@@ -4,7 +4,7 @@ mu_test <- c(0.1,0.15,0.2,0.25,0.3,0.4,.5,1,1.5,2,2.5,3,3.5,4,4.5,5)   #test val
 M <- length(mu_test)              #number of Monte Carlo replicas
 power <- numeric(M)               #vector to storage the empirrical power of the test
 nobs <- c(20, 40, 50, 100)        #sampling size
-power_nobs <- matrix(0,length(nobs),M) #matrix to storage the empirical power of the test for each n
+power_nobs <- matrix(0,length(nobs),M) #matrix to storage the empirical power of the test for each size n
 c <- 1
 for(j in nobs) {
   for (i in 1:M) {
@@ -19,7 +19,7 @@ for(j in nobs) {
   c = c+1
 }
 
-
+#Plots to see the performance varying n
 par(mfrow=c(2,2))
 plot(mu_test, power_nobs[1,], type = "l", xlab = bquote(theta), ylab = "Power", main = "n = 20")
 abline(v = mu_verd, lty = 1)
